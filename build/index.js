ff658fea9fe28d867b10f4341d7177464c545598
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-02-21 08:29:06 +0200

    Bubble sort algorithm

--- build/index.js
+++ build/index.js
@@ -3,7 +3,19 @@ class Sorter {
     constructor(collection) {
         this.collection = collection;
     }
-    sort() { }
+    sort() {
+        let max = this.collection.length;
+        while (max > 0) {
+            for (let i = 1; i < max; i++) {
+                if (this.collection[i - 1] > this.collection[i]) {
+                    let temp = this.collection[i - 1];
+                    this.collection[i - 1] = this.collection[i];
+                    this.collection[i] = temp;
+                }
+            }
+            max--;
+        }
+    }
 }
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    let max = this.collection.length;
+    while (max > 0) {
+      for (let i = 1; i < max; i++) {
+        if (this.collection[i - 1] > this.collection[i]) {
+          let temp = this.collection[i - 1];
+          this.collection[i - 1] = this.collection[i];
+          this.collection[i] = temp;
+        }
+      }
+      max--;
+    }
+  }
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
+
 console.log(sorter.collection);
